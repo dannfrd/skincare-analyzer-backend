4e99e668d7d7b3a3114f4537ecd8371a75a14a42
derika-dev <derikadea82@gmail.com>
2026-06-08 22:26:34 +0700

    update history and edit profile user

--- database/migration.sql
+++ database/migration.sql
@@ -173,6 +173,10 @@ ALTER TABLE analyses
     ADD COLUMN ai_model VARCHAR(100);
 ALTER TABLE analyses
     ADD COLUMN ai_output LONGTEXT;
+ALTER TABLE analyses
+    ADD COLUMN raw_result LONGTEXT;
+ALTER TABLE users
+    ADD COLUMN profile_picture VARCHAR(255) NULL AFTER firebase_uid;
 
 DELETE duplicate_detail
 FROM analysis_details duplicate_detail
@@ -183,7 +187,8 @@ INNER JOIN analysis_details retained_detail
 
 ALTER TABLE analysis_details
     ADD CONSTRAINT unique_analysis_ingredient
-    UNIQUE (analysis_id, ingredient_id);
+    UNIQUE (analysis_id,
+     ingredient_id);
 
 DELETE duplicate_history
 FROM user_histories duplicate_history
