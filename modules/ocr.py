8ccab9fea64b100b3deb3ed7592a6df09d4287fd
Ardan Ferdiansah <ardanferdiansah03@gmail.com>
2026-07-20 19:59:52 +0700

    fix: enforce paddle environment flags globally

--- modules/ocr.py
+++ modules/ocr.py
@@ -5,9 +5,13 @@ Menggunakan PaddleOCR untuk ekstraksi teks.
 Di-load sekali saat pertama dipanggil (lazy loading) dan menggunakan konfigurasi ringan (CPU only).
 """
 
+import os
+# Fix internal PaddlePaddle CPU bugs by disabling PIR and MKLDNN globally
+os.environ['FLAGS_enable_pir_api'] = '0'
+os.environ['FLAGS_use_mkldnn'] = '0'
+
 import numpy as np
 import logging
-import os
 import cv2
 
 logger = logging.getLogger(__name__)
@@ -18,10 +22,6 @@ def get_paddle_ocr():
     global paddle_ocr_instance
     if paddle_ocr_instance is None:
         try:
-            # Fix internal PaddlePaddle CPU bugs by disabling PIR and MKLDNN
-            os.environ['FLAGS_enable_pir_api'] = '0'
-            os.environ['FLAGS_use_mkldnn'] = '0'
-            
             from paddleocr import PaddleOCR
             # Konfigurasi ringan sesuai requirement VPS 2GB RAM: CPU only, english model
             logger.info("Initializing PaddleOCR...")
